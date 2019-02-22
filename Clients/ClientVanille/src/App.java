087e3efb88412fb635e08c9f9101ac59d7f3a431
ZaiWoK <46940508+ZaiWoK@users.noreply.github.com>
2019-02-21 19:52:34 -0500

    Messages aléatoire + Ajout d'une interface

--- Clients/ClientVanille/src/App.java
+++ Clients/ClientVanille/src/App.java
@@ -1,30 +1,24 @@
 import java.util.Iterator;
 import java.util.List;
+
 import accesseur.PenseeDAO;
 import modele.Pensee;
 import outils.Journal;
+import vue.VueInspirationVisuelle;
 
 public class App {
 
-	public static void main(String[] args) {
+	public static void main(String[] parametres) {
 
 		//Journal.activer();
 		Journal.activerNiveau(0);
 		
-		PenseeDAO penseeDAO = new PenseeDAO();
-		List<Pensee> listePensees = penseeDAO.listerPensees();
+		//Pensee pensee = new Pensee("Rossetti","Ce qui est plus triste qu une oeuvre inachevee, c est une oeuvre jamais commencee.");
+		//penseeDAO.ajouterPensee(pensee);		
 		
-		for(Iterator<Pensee> visiteur = listePensees.iterator(); visiteur.hasNext(); )
-		{
-			Pensee pensee = visiteur.next();		
-			Journal.ecrire(5, pensee.getMessage() + "(" + pensee.getAuteur() + ")");
-		}
-
-		Pensee pensee = new Pensee("Rossetti","Ce qui est plus triste qu une oeuvre inachevee, c est une oeuvre jamais commencee.");
-		penseeDAO.ajouterPensee(pensee);
-
+		VueInspirationVisuelle.launch(VueInspirationVisuelle.class, parametres);
+		
+		
 	}
-	
 
 }
-
